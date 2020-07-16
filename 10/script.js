f1c8b974654f926ccb6bbd6d07050e12e78d782b
DimaKreed <dimonvarch2001@gmail.com>
2020-07-16 14:05:21 +0300

    made task with array of strings

--- 10/script.js
+++ 10/script.js
@@ -26,4 +26,32 @@ let multiplyBy3 = nums.map(value => value*3);
 console.log(multiplyBy3);
 
 
+//- создать массив со словами на 15-20 элементов.
+//так як метод сорт змінює масив то я створю такий самий, щоб його сортувати а початковий не мутував
+let str=['Hi','I','am','studying','Js','now','It','is','interesting','I','like','it','very','much','Hope','you','like','it','too'],str2=[];
+for (let i = 0; i < str.length; i++) {
+str2[i]=str[i];
+}
+console.log(str);
+//-- отсортировать его по алфавиту в восходящем порядке.
+let strSort = str2.sort((a, b) => {
+    if(a.toLowerCase()<b.toLowerCase()) {return -1}
+    else { return 1}
+})
+console.log(strSort);
+
+//-- отсортировать его по алфавиту  в нисходящем порядке.
+let strSortReverse = str2.sort((a, b) => {
+    if(a.toLowerCase()>b.toLowerCase()) {return -1}
+})
+console.log(strSortReverse);
+
+//-- отфильтровать слова длиной менее 4х символов
+let less4 = str.filter(value => value.length>4);
+console.log(less4);
+//-- перебрать массив при помощи map() и получить новый массив в котором все значения будут со знаком "!" в конце
+let stringStrong = str.map(value => value+'!')
+console.log(stringStrong);
+
+
 
