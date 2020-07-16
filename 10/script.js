82c38a8f8d19f68430e71564342b83260921359b
DimaKreed <dimonvarch2001@gmail.com>
2020-07-16 17:23:20 +0300

    made task Sort array of usersWithAddress;made task with changes to own array of cars;made task of finding min&max index of key in array;

--- 10/script.js
+++ 10/script.js
@@ -34,16 +34,11 @@ str2[i]=str[i];
 }
 console.log(str);
 //-- отсортировать его по алфавиту в восходящем порядке.
-let strSort = str2.sort((a, b) => {
-    if(a.toLowerCase()<b.toLowerCase()) {return -1}
-    else { return 1}
-})
+let strSort = str2.sort((a, b) => (a.toLowerCase()<b.toLowerCase())?-1:1);
 console.log(strSort);
 
 //-- отсортировать его по алфавиту  в нисходящем порядке.
-let strSortReverse = str2.sort((a, b) => {
-    if(a.toLowerCase()>b.toLowerCase()) {return -1}
-})
+let strSortReverse = str2.sort((a, b) => (a<b)?-1:1)
 console.log(strSortReverse);
 
 //-- отфильтровать слова длиной менее 4х символов
@@ -170,4 +165,100 @@ console.log(filter7);
 //- сили більше 250  + виробник бмв
 let filter8 = cars.filter(value => value.power>250&&value.producer==='bmw');
 console.log(filter8);
+
+let usersWithAddress = [
+    {id: 1, name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+    {id: 2, name: 'petya', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 1}},
+    {id: 3, name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
+    {id: 4, name: 'olya', age: 28, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 90}},
+    {id: 5, name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
+    {id: 6, name: 'anya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 2}},
+    {id: 7, name: 'oleg', age: 28, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 22}},
+    {id: 8, name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
+    {id: 9, name: 'masha', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 12}},
+    {id: 10, name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+    {id: 11, name: 'max', age: 31, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}}
+    ];
+// //-- отсортировать его по id пользователей
+// let sort1 = usersWithAddress.sort((a, b) => a.id-b.id);
+// console.log(sort1);
+// //-- отсортировать его по id пользователей в обратном опрядке
+// let sort2 = usersWithAddress.sort((a, b) => b.id-a.id);
+// console.log(sort2);
+// //-- отсортировать его по возрасту пользователей
+// let sort3 = usersWithAddress.sort((a, b) => a.age-b.age);
+// console.log(sort3);
+// //-- отсортировать его по возрасту пользователей в обратном порядке
+// let sort4 = usersWithAddress.sort((a, b) => b.age-a.age);
+// console.log(sort4);
+// //-- отсортировать его по имени пользователей
+// let sort5=usersWithAddress.sort((a, b) => (a.name.toLowerCase()<b.name.toLowerCase())?-1:1)
+// console.log(sort5);
+// //-- отсортировать его по имени пользователей в обратном порядке
+// let sort6=usersWithAddress.sort((a, b) =>(b.name.toLowerCase()<a.name.toLowerCase())?-1:1)
+// console.log(sort6);
+// //-- отсортировать его по названию улицы  в алфавитном порядке
+// let sort7=usersWithAddress.sort((a, b) =>  (a.address.street.toLowerCase()<b.address.street.toLowerCase())?-1:1);
+// console.log(sort7);
+// //-- отсортировать его по номеру дома по возрастанию
+// let sort8 = usersWithAddress.sort((a, b) => a.address.number-b.address.number);
+// console.log(sort8);
+//-- отфильтровать (оставить) тех кто младше 30
+let filter9 = usersWithAddress.filter(value => value.age<30);
+console.log(filter9);
+//-- отфильтровать (оставить) тех у кого отрицательный статус
+let filter10 = usersWithAddress.filter(value => value.status===false);
+console.log(filter10);
+//-- отфильтровать (оставить) тех у кого отрицательный статус и младше 30 лет
+let filter11 = usersWithAddress.filter(value => value.status===false&&value.age<30);
+console.log(filter11);
+//-- отфильтровать (оставить) тех у кого номер дома четный
+let filter12 = usersWithAddress.filter(value => value.address.number%2===0);
+console.log(filter12);
+
+
+//Створити обєкт автомобіля з полями:
+// Марка автомобля, потужність двигуна, власник, ціна, рік випуску.
+// Власник автомобіля теж має бути обєкт, у якого є поля
+//Для початку вкладіть всі наші створені автомобілі в масив cars.
+cars2=[
+    {producer:'bmw',power:200,price:5000,yearOfProduction:2010,owner:{name:'Vasyl',age:25,drivingExp:5}},
+    {producer:'mercedes',power:176,price:6250,yearOfProduction:2011,owner:{name:'Volodymyr',age:32,drivingExp:1}},
+    {producer:'audi',power:180,price:7200,yearOfProduction:2003,owner:{name:'Olexandr',age:22,drivingExp:2}},
+    {producer:'opel',power:153,price:4500,yearOfProduction:2005,owner:{name:'Petro',age:34,drivingExp:3}},
+    {producer:'toyota',power:185,price:9240,yearOfProduction:2012,owner:{name:'Maria',age:23,drivingExp:4}},
+    {producer:'renault',power:146,price:4680,yearOfProduction:2007,owner:{name:'Dmytro',age:19,drivingExp:1}},
+    {producer:'cherry',power:150,price:3100,yearOfProduction:2004,owner:{name:'Olesya',age:39,drivingExp:12}},
+    {producer:'ford',power:166,price:4000,yearOfProduction:2013,owner:{name:'Iryna',age:40,drivingExp:4}},
+    {producer:'mazda',power:183,price:6750,yearOfProduction:2015,owner:{name:'Ivan',age:36,drivingExp:11}},
+    {producer:'nissan',power:190,price:3450,yearOfProduction:2008,owner:{name:'Ruslan',age:29,drivingExp:9}},
+    {producer:'honda',power:174,price:2200,yearOfProduction:2009,owner:{name:'Oksana',age:27,drivingExp:5}},
+]
+//Далі необхідно рати кожну другу машинку (цикл з кроком в 2), та робити їй підвищення потужності двигуна на 10% та ціну на 5%
+let carsAfterUpdate=cars2.map((value, index) =>{
+    if(index%2===0)
+    {
+       value.power*=1.1;
+        value.price*=1.05;
+        console.log(value.producer +" is updated");
+        return value;
+    }
+    return value;
+} )
+//Після того зробити перевірку досвіду ВСІХ наших водіїв. Якщо досвід водія менший за 5 років, але його вік більший за 25, то необідно відправити його на курси підвищення кваліфікації, що збільшить йому досвід на 1 рік.
+carsAfterUpdate.forEach((value, index) =>{if (value.owner.drivingExp<5&&value.owner.age>25){console.log(value.owner.name+" is going for cources"); value.owner.drivingExp+=1;}});
+//Також спробуйте порахувати суму, яку потрібно потратити для покупки всіх цих авто в циклі
+let price=carsAfterUpdate.reduce((accum, currentValue) => accum+currentValue.price,0);
+console.log(price);
+
+//Задача: дан отсортированный по возрастанию массив целых чисел. Необходимо вернуть наименьший и наибольший индекс заданного элемента.
+// Входные данные: arr — массив целых чисел значения которых по модулю не больше 10. Размер массива не более 10 элементов.
+// Вывод: наибольший и наименьший индекс в массиве заданного элемента. Если такого элемента нет в массиве, выведите -1.
+let Arr = [1, 2, 3, 4, 4, 4, 4, 7, 7, 9, 14];
+
+function minMaxIndex(arr) {
+   let key=+prompt("Enter key",1);
+    return 'MinIndex = '+ arr.indexOf(key)+', MaxIndex = '+arr.lastIndexOf(key);
+}
+console.log(minMaxIndex(Arr));
 
