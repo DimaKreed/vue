37557204f434aca536f71f2cbefaf774d49c575c
DimaKreed <dimonvarch2001@gmail.com>
2020-07-22 09:28:39 +0300

    Add rules.html & created scriptForRules.js

---
+++ 11/scriptForRules.js
@@ -0,0 +1,16 @@
+//-- создать скрипт, который берет считывает на странице (rules.html) текст и делает сбоку меню-оглавление по всем заголовкам которые есть в тексте.
+// При клике на пункт оглавления вы должны отправляться к этому пункту в тексте
+
+//117 - є сторінка rules.html. Контентом сторінки є заголовки та параграфи. Заголовки (h2) характеризують тему контенту яка вказана в параграфі.
+// створити скрипт, котрий зчитує всі заголовки, та робить в блоці з id=content з них список(ul>li), який буде змістом того, що знаходиться на сторінці.
+// Скріпт повинен працювати навіть якщо кількість блоків з заголовком та параграфом зміниться.
+let ul=document.createElement('ul');
+let h2Text=document.getElementsByTagName('h2');
+for(let h2 of h2Text)
+{
+    let li=document.createElement('li');
+    li.innerText=h2.textContent;
+    ul.appendChild(li);
+}
+let blockIdContent=document.getElementById('content');
+blockIdContent.appendChild(ul);
