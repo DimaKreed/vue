fc5fb63d02b78cb1473f7dd6f4821c45567ff832
DimaKreed <dimonvarch2001@gmail.com>
2020-07-28 16:43:08 +0300

    made part2(promise) & part3 (async+await)

--- Callback_Promise_AsyncAwait/script.js
+++ Callback_Promise_AsyncAwait/script.js
@@ -1,3 +1,19 @@
+//Треба реалізувати свій розпорядок дня.
+// Колбеками, промісами та асинк авейт.
+//
+// В дні має бути від 7 до 10 подій. Всі події мають мати описані успішні та не успішні варіанти виконання.
+// Має бути так
+// 1) прокинувся
+// 2) Поснідав
+// 3) почистав зуби
+// і т.д.
+//
+// Якщо щось пішло не так (нема шо їсти), то має бути викинута помилка і решта функцій виконуватись не мають.
+// Якщо ж все ок, то ви маєте прожити свій звичайний день.
+// Кожна подія має бути з рандомною (не по зростанню) затримкою.
+
+
+
 // function wakeUp(wasAlarm,cb)
 // {
 //     setTimeout(()=>
@@ -80,7 +96,7 @@
 let div=document.createElement("div");
 document.body.appendChild(div);
 
-function dayFailed(){document.write('Day failed')};
+function dayFailed(){document.write('Day failed')}
 //вставляє картинку і текст який отримує з колбеку
 function createBlockImageAndText(url,text){
     let img=document.createElement("img");
@@ -91,10 +107,12 @@ function createBlockImageAndText(url,text){
     div.appendChild(p);
 }
 
-//час від 1 до 5 секунд
+//час від 1000 до 5000 мілісекунд
 function randomTimeout(){
     return 1000-0.5+Math.random()* (5000-1000+1);
 }
+
+
 //CALLBACK HELL
 //HELP HELP HElP
 //                                                      ||     this  looks so terrible & unreadable
@@ -147,7 +165,7 @@ function randomTimeout(){
 //                                                                        else
 //                                                                        {
 //                                                                            createBlockImageAndText( 'https://st4.depositphotos.com/4472335/25330/v/1600/depositphotos_253305018-stock-illustration-family-sitting-at-dining-table.jpg', data);
-//                                                                            goBed('YOU SHOULD GO TO THE BED',(error,data)=>
+//                                                                            goBed(confirm('YOU SHOULD GO TO THE BED'),(error,data)=>
 //                                                                                {
 //                                                                                    if(error) dayFailed();
 //                                                                                    else
@@ -190,80 +208,178 @@ function randomTimeout(){
 // )
 //
 
-function wakeUp(wasAlarm,cb)
-{
+function wakeUpPromise(wasAlarm)
+{ return new Promise((resolve, reject) => {
     setTimeout(()=>
     {
-        (wasAlarm)?cb(null,'Good MO'):cb('OOOOPS')
+        (wasAlarm)?resolve('Good MO'):reject('OOOOPS')
     },randomTimeout())
+
+})
 
 }
 
-function cleanTeeth(wasWater,cb)
-{
+function cleanTeethPromise(wasWater)
+{return new Promise((resolve, reject) => {
     setTimeout(()=>
     {
-        (wasWater)?cb(null,'Teeth are clean'):cb('OOPS')
+        (wasWater)?resolve('Teeth are clean'):reject('OOPS')
     },randomTimeout())
+})
+
 
 }
 
-function eatBreakfest(wasMilk,cb)
-{
+function eatBreakfestPromise(wasMilk)
+{return new Promise((resolve, reject) => {
     setTimeout(()=>
     {
-        (wasMilk)?cb(null,'Yummy'):cb('OOPS')
+        (wasMilk)?resolve('Yummy'):reject('OOPS')
     },randomTimeout())
+})
+
 
 }
 
-function startCoding(wasTask,cb)
-{
+function startCodingPromise(wasTask)
+{return new Promise((resolve, reject) => {
     setTimeout(()=>
     {
-        (wasTask)?cb(null,"Let's go!"):cb('OOPS')
+        (wasTask)?resolve("Let's go!"):reject('OOPS')
     },randomTimeout())
-}
-
-function takeBreak(wannaBeHealthy,cb)
-{
-    setTimeout(()=>
-    {
-        (wannaBeHealthy)?cb(null,"You were needed for the rest"):cb('OOPS')
-    },randomTimeout())
-}
-
-function eatDinner(isSmthToEat,cb)
-{
-    setTimeout(()=>
-    {
-        (isSmthToEat)?cb(null,"It was tasty"):cb('OOPS')
-    },randomTimeout())
-}
-
-function goForWalk(goodWeather,cb)
-{
-    setTimeout(()=>
-    {
-        (goodWeather)?cb(null,'See your friends'):cb('OOPS')
-    },randomTimeout())
+})
 
 }
 
-function eatSupper(isSmthToEat,cb)
-{
+function takeBreakPromise(wannaBeHealthy)
+{return new Promise((resolve, reject) => {
     setTimeout(()=>
     {
-        (isSmthToEat)?cb(null,'Really good taste'):cb('OOPS')
+        (wannaBeHealthy)?resolve("You were needed for the rest"):reject('OOPS')
     },randomTimeout())
+})
 
 }
 
-function goBed(needSleep,cb)
-{
+function eatDinnerPromise(isSmthToEat)
+{return new Promise((resolve, reject) => {
     setTimeout(()=>
     {
-        (needSleep)?cb(null,'Have a great dreams'):cb('OOPS')
+        (isSmthToEat)?resolve("It was tasty"):reject('OOPS')
     },randomTimeout())
+})
 
 }
+
+function goForWalkPromise(goodWeather)
+{return new Promise((resolve, reject) => {
+    setTimeout(()=>
+    {
+        (goodWeather)?resolve('See your friends'):reject('OOPS')
+    },randomTimeout())
+})
+
+
+}
+
+function eatSupperPromise(isSmthToEat)
+{return new Promise((resolve, reject) => {
+    setTimeout(()=>
+    {
+        (isSmthToEat)?resolve('Really good taste'):reject('OOPS')
+    },randomTimeout())
+})
+}
+
+function goBedPromise(needSleep)
+{   return new Promise((resolve, reject) => {
+    setTimeout(()=>
+    {
+        (needSleep)?resolve('Have a great dreams'):reject('OOPS')
+    },randomTimeout())
+})
+}
+
+// wakeUpPromise(confirm('Do the alarm clock make din-din'))
+//     .then(value => {
+//         createBlockImageAndText( 'https://startupanz.com/wp-content/uploads/2020/05/how-to-wake-up-early-696x319.png', value);
+//        return  cleanTeethPromise(confirm('Was water in your tap'))
+//            .catch(reason => {dayFailed()})
+//     })
+//
+//     .then(value => {
+//         createBlockImageAndText( 'https://image.freepik.com/free-vector/clean-tooth-background_1270-86.jpg', value);
+//           return   eatBreakfestPromise(confirm('Do you have milk for flakes?'))
+//           .catch(reason => {dayFailed()})
+//     })
+//     .then(value => {
+//         createBlockImageAndText( 'https://i.pinimg.com/originals/1f/4f/cd/1f4fcd32ac128940596ed1392fa1a2d0.jpg', value);
+//            return  startCodingPromise(confirm('Do you have the task for coding?'))
+//            .catch(reason => {dayFailed()})
+//     })
+//     .then(value => {
+//         createBlockImageAndText( 'https://cdn.worldvectorlogo.com/logos/logo-javascript.svg', value);
+//        return takeBreakPromise(confirm('Do you wanna stay healthy and take a break?'))
+//        .catch(reason => {dayFailed()})
+//     })
+//     .then(value => {
+//         createBlockImageAndText( 'https://image.freepik.com/free-vector/take-break_1343-435.jpg', value);
+//         return eatDinnerPromise(confirm('Is there smth to eat for dinner?'))
+//         .catch(reason => {dayFailed()})
+//     })
+//     .then(value => {
+//         createBlockImageAndText( 'https://i.pinimg.com/originals/52/ca/92/52ca929cee72cbf7b08d4c61e285e478.jpg', value);
+//         return goForWalkPromise(confirm('Is it a good weather?'))
+//         .catch(reason => {dayFailed()})
+//     })
+//     .then(value => {
+//         createBlockImageAndText( 'https://previews.123rf.com/images/scusi/scusi1703/scusi170300015/74265340-people-go-for-a-walk-in-the-park.jpg', value);
+//        return eatSupperPromise(confirm('Is there smth to eat for supper?'))
+//        .catch(reason => {dayFailed()})
+//     })
+//     .then(value => {
+//         createBlockImageAndText( 'https://st4.depositphotos.com/4472335/25330/v/1600/depositphotos_253305018-stock-illustration-family-sitting-at-dining-table.jpg', value);
+//         return goBedPromise(confirm('YOU SHOULD GO TO THE BED'))
+//             .catch(reason => {dayFailed()})
+//     })
+//     .then(value => {
+//         createBlockImageAndText( 'https://images.assetsdelivery.com/compings_v2/yupiramos/yupiramos1709/yupiramos170902874.jpg', value);
+//     })
+//     .finally(() => {
+//         createBlockImageAndText('https://i.pinimg.com/originals/f7/f7/99/f7f799fba63ba0dc965837454e62c3b8.jpg','End of the day');
+//     })
+//     .catch(reason => { dayFailed()});
+
+
+
+async function myDaySchedule() {
+    try {
+
+        createBlockImageAndText('https://startupanz.com/wp-content/uploads/2020/05/how-to-wake-up-early-696x319.png',await wakeUpPromise(confirm('Do the alarm clock make din-din')));
+
+        createBlockImageAndText('https://image.freepik.com/free-vector/clean-tooth-background_1270-86.jpg', await cleanTeethPromise(confirm('Was water in your tap')));
+
+        createBlockImageAndText('https://i.pinimg.com/originals/1f/4f/cd/1f4fcd32ac128940596ed1392fa1a2d0.jpg',await eatBreakfestPromise(confirm('Do you have milk for flakes?')));
+
+        createBlockImageAndText('https://cdn.worldvectorlogo.com/logos/logo-javascript.svg',await startCodingPromise(confirm('Do you have the task for coding?')));
+
+        createBlockImageAndText('https://image.freepik.com/free-vector/take-break_1343-435.jpg',await takeBreakPromise(confirm('Do you wanna stay healthy and take a break?')));
+
+        createBlockImageAndText('https://i.pinimg.com/originals/52/ca/92/52ca929cee72cbf7b08d4c61e285e478.jpg',await eatDinnerPromise(confirm('Is there smth to eat for dinner?')));
+
+        createBlockImageAndText('https://previews.123rf.com/images/scusi/scusi1703/scusi170300015/74265340-people-go-for-a-walk-in-the-park.jpg',await goForWalkPromise(confirm('Is it a good weather?')));
+
+        createBlockImageAndText('https://st4.depositphotos.com/4472335/25330/v/1600/depositphotos_253305018-stock-illustration-family-sitting-at-dining-table.jpg',await eatSupperPromise(confirm('Is there smth to eat for supper?')));
+
+        createBlockImageAndText('https://images.assetsdelivery.com/compings_v2/yupiramos/yupiramos1709/yupiramos170902874.jpg',await goBedPromise(confirm('YOU SHOULD GO TO THE BED')));
+
+        createBlockImageAndText('https://i.pinimg.com/originals/f7/f7/99/f7f799fba63ba0dc965837454e62c3b8.jpg', 'End of the day');
+    }
+    catch (e) {
+        console.error(e);
+        dayFailed();
+    }
+
+}
+
+myDaySchedule();
