69e4f52bf8ec7701649a5a9b258920bbebce689b
DimaKreed <dimonvarch2001@gmail.com>
2020-08-23 13:04:20 +0300

    Made additional task

---
+++ vue/Second Hometask/additionaltask/src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <h3>E-mail</h3>
+    <input type="text" v-model="eMail">
+
+    <h3>Password</h3>
+    <input type="password" v-model="password">
+
+    <h3>Gender</h3>
+    <div>
+      <input type="radio" value="male" name="gender" v-model="gender"  ><span>male</span>
+      <input type="radio" value="female" name="gender" v-model="gender"><span>female</span>
+    </div>
+
+
+    <h3>Country</h3>
+    <select v-model="country">
+      <option v-for="(country,i) in countries" :key="i" >{{country}}</option>
+    </select>
+
+     <h3>Languages</h3>
+    <input type="checkbox" id="JS" value="JS" v-model="languages" ><label for="JS">JS</label>
+    <input type="checkbox" id="Java" value="Java" v-model="languages"><label for="Java">Java</label>
+    <input type="checkbox" id="Python" value="Python" v-model="languages"><label for="Python">Python</label>
+
+      <button @click="createAccount()">Send form</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data(){
+
+    return{
+      countries : ['Afghanistan', 'Albania', 'Algeria', 'American Samoa', 'Andorra', 'Angola', 'Anguilla', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bangladesh', 'Barbados', 'Bahamas', 'Bahrain', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'British Indian Ocean Territory', 'British Virgin Islands', 'Brunei Darussalam', 'Bulgaria', 'Burkina Faso', 'Burma', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Cayman Islands', 'Central African Republic', 'Chad', 'Chile', 'China', 'Christmas Island', 'Cocos (Keeling) Islands', 'Colombia', 'Comoros', 'Congo-Brazzaville', 'Congo-Kinshasa', 'Cook Islands', 'Costa Rica', 'Croatia', 'Cura?ao', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'East Timor', 'Ecuador', 'El Salvador', 'Egypt', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Federated States of Micronesia', 'Fiji', 'Finland', 'France', 'French Guiana', 'French Polynesia', 'French Southern Lands', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guadeloupe', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Heard and McDonald Islands', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Martinique', 'Mauritania', 'Mauritius', 'Mayotte', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Niue', 'Norfolk Island', 'Northern Mariana Islands', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Pitcairn Islands', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'R?union', 'Romania', 'Russia', 'Rwanda', 'Saint Barth?lemy', 'Saint Helena', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Martin', 'Saint Pierre and Miquelon', 'Saint Vincent', 'Samoa', 'San Marino', 'S?o Tom? and Pr?ncipe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Sint Maarten', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Georgia', 'South Korea', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Svalbard and Jan Mayen', 'Sweden', 'Swaziland', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Togo', 'Tokelau', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks and Caicos Islands', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Vatican City', 'Vietnam', 'Venezuela', 'Wallis and Futuna', 'Western Sahara', 'Yemen', 'Zambia', 'Zimbabwe'],
+      eMail:'',
+      password:'',
+      gender:'male',
+      country:'Ukraine',
+      languages:['JS'],
+      id:0
+    }
+  },
+  computed:{
+
+  },
+
+  methods:{
+    createAccount(){
+      if(this.eMail&&this.password)
+      {let user={
+        id:this.id,
+        eMail:this.eMail,
+      password: this.password,
+        gender:this.gender,
+        country: this.country,
+        languages: this.languages
+      }
+      this.id++;
+
+      console.log(user)
+      this.eMail='';
+      this.password='';
+      this.gender='male';
+      this.country='Ukraine';
+      this.languages=['JS'];
+      alert("Ваші дані успішно відправлені")
+      }
+
+      else alert("Деякі поля не введені")
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
