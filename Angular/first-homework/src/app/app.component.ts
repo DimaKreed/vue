36eaa030ce0c31084544f0bb6d0495da4c95e0cd
DimaKreed <dimonvarch2001@gmail.com>
2020-09-09 11:23:17 +0300

    Made 1 task(get user from jsp) angular

--- Angular/first-homework/src/app/app.component.ts
+++ Angular/first-homework/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {UserService} from "./services/user.service";
+import {UserModel} from "./models/user.model";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private userService:UserService) {
+    this.userService.getUsers().subscribe(users=>{
+      this.users=users;
+    })
+
+  }
+
   title = 'first-homework';
+  users:UserModel[]=[];
 }
---
+++ Angular/first-homework/src/app/models/user.model.ts
@@ -0,0 +1,8 @@
+export interface UserModel {
+  email:string;
+  id:number;
+  name:string;
+  phone:string;
+  userName:string;
+  website:string;
+}
