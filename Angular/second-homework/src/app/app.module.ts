f3e6afd27dda1a812d186f748cbb10ada7b2df7c
DimaKreed <dimonvarch2001@gmail.com>
2020-09-10 18:50:08 +0300

    connected httpClientModule

--- Angular/second-homework/src/app/app.module.ts
+++ Angular/second-homework/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FullTodoListComponent } from './components/full-todo-list/full-todo-list.component';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { UpdateTodoComponent } from './components/update-todo/update-todo.component';
+import { DeleteTodoComponent } from './components/delete-todo/delete-todo.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FullTodoListComponent,
+    AddTodoComponent,
+    UpdateTodoComponent,
+    DeleteTodoComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
