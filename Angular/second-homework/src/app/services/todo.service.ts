0eb7dc2176c902edde415298d1b3f321aae22d9e
DimaKreed <dimonvarch2001@gmail.com>
2020-09-10 18:34:55 +0300

    created service to get todoList from firebase

---
+++ Angular/second-homework/src/app/services/todo.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {TodoModel} from "../models/todo.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  constructor(private http:HttpClient) {
+
+  }
+
+  getListOfTodo():Observable<TodoModel[]>{
+    return this.http.get<TodoModel[]>('https://myvueproject-2080c.firebaseio.com/.json')
+}
+
+  // getUsers():Observable<UserModel[]>{
+//     return this.http.get<UserModel[]>('https://jsonplaceholder.typicode.com/users');
+//   }
+}
