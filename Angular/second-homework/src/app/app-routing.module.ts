578e8af561aa28c9c78fd0e034ddedb72d989ea5
DimaKreed <dimonvarch2001@gmail.com>
2020-09-10 20:20:29 +0300

    created routes

--- Angular/second-homework/src/app/app-routing.module.ts
+++ Angular/second-homework/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {FullTodoListComponent} from "./components/full-todo-list/full-todo-list.component";
+import {AddTodoComponent} from "./components/add-todo/add-todo.component";
+import {AuthorComponent} from "./components/author/author.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'allTodo',component:FullTodoListComponent},
+  {path:'addTodo',component:AddTodoComponent},
+  {path:'author',component:AuthorComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
