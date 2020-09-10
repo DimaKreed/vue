91488b0f4f6a4075c9fffbc6e966ea1a43ae6a89
DimaKreed <dimonvarch2001@gmail.com>
2020-09-10 18:49:24 +0300

    created components

---
+++ Angular/second-homework/src/app/components/full-todo-list/full-todo-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-full-todo-list',
+  templateUrl: './full-todo-list.component.html',
+  styleUrls: ['./full-todo-list.component.css']
+})
+export class FullTodoListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
