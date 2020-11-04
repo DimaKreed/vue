547f5e20f7772505306bca93b8e18119f820e197
DimaKreed <dimonvarch2001@gmail.com>
2020-11-04 15:55:30 +0200

    Create methods for getting posts,users,comments

--- React/2hometask/src/services/JSONPlaceholder.js
+++ React/2hometask/src/services/JSONPlaceholder.js
@@ -2,8 +2,21 @@ import React, {Component} from 'react';
 
 class JsonPlaceholder extends Component {
   URL='https://jsonplaceholder.typicode.com/users';
+  PostUrl='https://jsonplaceholder.typicode.com/posts';
+  CommentsUrl='https://jsonplaceholder.typicode.com/comments';
   async getUsers(){
     return (await fetch(this.URL)).json();
+  }
+
+  async getPostsOfUser(id){
+    return (await fetch(`${this.URL}/${id}/posts`)).json();
+  }
+  async getAllPosts(){
+    return (await fetch(this.PostUrl)).json();
+  }
+  async getCommentsOfPosts()
+  {
+    return (await fetch(this.CommentsUrl)).json();
   }
 }
 
