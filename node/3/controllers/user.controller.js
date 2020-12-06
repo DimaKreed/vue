a39a362d779ac4cd90522774ddb78af7930225e1
DimaKreed <dimonvarch2001@gmail.com>
2020-12-06 18:19:16 +0200

    Deleted unnecessary files

--- node/3/controllers/user.controller.js
+++
@@ -1,8 +0,0 @@
-// const { userService } = require('../services');
-
-module.exports = {
-    // getUserById: userService.getUserById,
-    // deleteUser: userService.deleteUser,
-    // updateUser: userService.updateUser,
-    // createUser: userService.createUser
-};
