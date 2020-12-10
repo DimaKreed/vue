4c39c06bcbe741061770192dab5e16dfb479a0c2
DimaKreed <dimonvarch2001@gmail.com>
2020-12-11 00:40:17 +0200

    used routed for 'cars'

--- node/3/App.js
+++ node/3/App.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes');
-const db = require('./dataBase').getInstance();
+const db = require('./database').getInstance();
 
 const app = express();
 
@@ -10,8 +10,11 @@ const port = 5000;
 
 app.use(express.static(path.join(process.cwd(), 'dataForAll')));
 app.use(express.json());
+
 app.listen(port, () => {
+    console.log(`App listen port ${port}`);
 });
-console.log(`App listen port ${port}`);
+
 app.use('/users', routes.usersRouter);
 app.use('/error', routes.errorRouter);
+app.use('/cars', routes.carsRouter);
