ec4c3c814b277f7acc3dec8965ab7c12b1b114bb
DimaKreed <dimonvarch2001@gmail.com>
2020-12-11 15:21:12 +0200

    Added new middlewares,changed errors catching

--- node/3/middlewares/users.middleware.js
+++ node/3/middlewares/users.middleware.js
@@ -1,4 +1,6 @@
 const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const { ErrorHandler, errors: { NOT_VALID_BODY, NOT_VALID_ID, NOT_FOUND } } = require('../database/errors');
+const { usersService } = require('../services');
 
 module.exports = {
     checkUserValidity: (req, res, next) => {
@@ -6,25 +8,48 @@ module.exports = {
             name, email, age, password
         } = req.body;
         try {
-            if (!name || !password || !email || !age) throw new Error('user is not valid');
-            if (name.length < 4) throw new Error('name should have at least 4 symbols');
-            if (!re.test(email)) throw new Error('email is not valid');
-            if (age < 1) throw new Error('age should have at least 1 year');
+            if (!name || !password || !email || !age) throw new ErrorHandler(NOT_VALID_BODY.code, NOT_VALID_BODY.message);
+            if (name.length < 4) throw new ErrorHandler(NOT_VALID_BODY.code, NOT_VALID_BODY.message);
+            if (!re.test(email)) throw new ErrorHandler(NOT_VALID_BODY.code, NOT_VALID_BODY.message);
+            if (age < 1) throw new ErrorHandler(NOT_VALID_BODY.code, NOT_VALID_BODY.message);
+            req.user = {
+                name, email, age, password
+            };
+            next();
         } catch (e) {
-            res.status(400).json(e.message);
+            next(e);
         }
-        next();
     },
 
     checkUserIdValidity: (req, res, next) => {
-        const { id } = req.params;
         try {
-            if (!id || id < 0) throw new Error('User id is not valid');
+            const { id } = req.params;
+            if (!id || id < 0) throw new ErrorHandler(NOT_VALID_ID.code, NOT_VALID_ID.message);
+            req.id = id;
+            next();
         } catch (e) {
-            res.status(200).json(e.message);
+            next(e);
         }
-        next();
-    }
-    ,
+    },
+    checkIsUsersGot: async (req, res, next) => {
+        try {
+            const users = await usersService.getUsers();
+            if (!users) throw new ErrorHandler(NOT_FOUND.code, NOT_FOUND.message);
+            req.users = users;
+            next();
+        } catch (e) {
+            next(e);
+        }
+    },
+    checkIsUserGot: async (req, res, next) => {
+        try {
+            const user = await usersService.getUserById(req.id);
+            if (!user) throw new ErrorHandler(NOT_FOUND.code, NOT_FOUND.message);
+            req.user = user;
+            next();
+        } catch (e) {
+            next(e);
+        }
+    },
 
 };
