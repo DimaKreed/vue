d1c06504d8e66b36405ed536945624c804a04355
DimaKreed <dimonvarch2001@gmail.com>
2020-12-11 15:22:40 +0200

    new errors

--- node/3/database/errors/errors.js
+++ node/3/database/errors/errors.js
@@ -1,4 +1,4 @@
-const { BAD_REQUEST } = require('./error-codes');
+const { BAD_REQUEST, NOT_FOUND } = require('./error-codes');
 
 module.exports = {
     NOT_VALID_BODY: {
@@ -8,5 +8,9 @@ module.exports = {
     NOT_VALID_ID: {
         message: 'ID is not valid',
         code: BAD_REQUEST
+    },
+    NOT_FOUND: {
+        message: 'DATA is not fount',
+        code: NOT_FOUND
     }
 };
